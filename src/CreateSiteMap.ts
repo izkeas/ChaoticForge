00ffd42e7a9d40383117de8a324a979e99d738a4
izkeas <izkeasyomito@gmail.com>
2023-02-05 12:58:09 -0300

    FIxed SITEMAP XML

--- src/CreateSiteMap.ts
+++ src/CreateSiteMap.ts
@@ -22,7 +22,7 @@ export default function createSiteMap(path : string = "./frontend/pages/**/*.tsx
     console.log("Building SiteMap...");
     let xml = 
     `<?xml version="1.0" encoding="UTF-8"?>\n\n` +
-    `<urlset xlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
     `\t{{URLS HERE}}\n` +
     `</urlset>\n`
 
